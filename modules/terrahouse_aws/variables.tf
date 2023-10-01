d4ab7394d42ef7b4790731518167eaa232c734ab
chivondo <125144442+chivondo@users.noreply.github.com>
2023-10-01 18:07:31 -0500

    #31 only upload index.html if a content version has change (#32)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -38,3 +38,13 @@ variable "error_html_filepath" {
     error_message = "The specified error_html_filepath does not exist."
   }
 }
+
+variable "content_version" {
+  type        = number
+  description = "Positive integer representing the content version"
+
+  validation {
+    condition     = var.content_version > 0 && ceil(var.content_version) == var.content_version
+    error_message = "Content version must be a positive integer starting at 1."
+  }
+}
