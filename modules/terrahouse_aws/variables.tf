9aa02899cf245667e351592f061f762f0814e514
chivondo <125144442+chivondo@users.noreply.github.com>
2023-10-07 13:20:20 -0500

    #35 upload assets using a for_each (#36)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -48,3 +48,9 @@ variable "content_version" {
     error_message = "Content version must be a positive integer starting at 1."
   }
 }
+
+variable "assets_path" {
+  description = "Path to assets folder"
+  type = string
+  
+}
