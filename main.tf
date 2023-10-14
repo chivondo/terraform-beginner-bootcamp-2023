30c079e124827410a21b7cda705950a2b971f24b
chivondo <veral.jose@gmail.com>
2023-10-14 22:49:45 +0000

    #39 finish implementation of tf provider

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ terraform {
 }
 
 provider "terratowns"{
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -30,3 +30,15 @@ provider "terratowns"{
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = " How to play, with all the toys"
+  description = <<DESCRIPTION
+Any description you want
+  DESCRIPTION
+  //domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+  
+}
